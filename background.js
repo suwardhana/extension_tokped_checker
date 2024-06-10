865bcd172679fc0ebc5e08b5c0ceb82b5c373c3a
Suwardhana <suwar_dhana@yahoo.co.id>
2024-06-10 14:46:09 +0700

    done functionality

--- background.js
+++ background.js
@@ -1,7 +1,2 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Hello background');
-  chrome.action.setBadgeText({
-    text: "OFF",
-  });
-});
+
 
--- handle_data.js
+++ handle_data.js
@@ -1,7 +1,8 @@
 document.getElementById("save_link").addEventListener("click", saveUrl);
 document.getElementById("clear_all_link").addEventListener("click", clearAllLink);
 
-showAllLink();
+// showAllLink(); // uncomment this line to show all link from storage
+document.addEventListener("DOMContentLoaded", fetchFirst);
 
 function saveUrl() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -23,17 +24,16 @@ function saveUrl() {
     // get local storage item url list
     var allUrls = JSON.parse(localStorage.getItem("urlList"));
     console.log(allUrls);
-    alert("URL saved: " + currentUrl);
-    showAllLink();
+    alert("URL saved");
+    fetchFirst();
   });
 }
 
 function clearAllLink() {
   localStorage.clear();
-  showAllLink();
 }
 
-function showAllLink() {
+/* function showAllLink() {
   // clear content of #show_link_here, and then show all link from storage as ul li
   var showLinkHere = document.getElementById("list_available_link");
   showLinkHere.innerHTML = "";
@@ -67,7 +67,7 @@ function showAllLink() {
       showLinkHere.appendChild(link);
     };
   }
-}
+} */
 
 function getCleanTitle(title) {
   const inputString = title
@@ -83,9 +83,88 @@ function getCleanTitle(title) {
 
 function removeItemFromStoragebyIndex(index) {
   console.log(index);
+  alert('deleted index' + index);
   var allUrls = JSON.parse(localStorage.getItem("urlList"));
   allUrls.splice(index, 1);
   localStorage.setItem("urlList", JSON.stringify(allUrls));
-  showAllLink();
+  fetchFirst();
 }
+
+function fetchFirst() {
+  // ajax to https://imtheluckiest.com/shopee/tokped/get_to_post.php GET with param data_link[] from urlList
+  var urlList = JSON.parse(localStorage.getItem("urlList"));
+  if (urlList == null) return;
+  var dataLinkArray = urlList.map(function (urlObject) {
+    return urlObject.url;
+  });
+  var showLinkHere = document.getElementById("list_available_link");
+  var showNotAvailableLinkHere = document.getElementById("list_not_available_link");
+  showLinkHere.innerHTML = "";
+  showNotAvailableLinkHere.innerHTML = "";
+  $.ajax({
+    url: "https://imtheluckiest.com/shopee/tokped/get_to_post.php",
+    type: "GET",
+    data: $.param({ data_link: dataLinkArray }),
+    traditional: true,
+    success: function (response) {
+      // return index that available ex = [0,5,10]
+      // decode json
+      var indexToShow = JSON.parse(response);
+      // show urlList with index from indexToShow
+      indexToShow.forEach(index => {
+        var link = document.createElement("div");
+        link.className = "list-group-item";
+        link.style = "cursor : pointer";
+        link.dataset.linkUrl = urlList[index].url;
+        link.dataset.linkTitle = urlList[index].title;
+        link.innerHTML = `<div class="d-flex justify-content-between align-items-center">
+        <div class="title-link">
+          ${urlList[index].title}
+        </div>
+        <div>
+          <i class="fa fa-remove fa-fw"></i>
+        </div>
+      </div>`;
+
+        var titleLink = link.querySelector(".title-link");
+        titleLink.addEventListener("click", function (event) {
+          event.preventDefault();
+          var url_new_tab = this.parentNode.parentNode.dataset.linkUrl;
+          chrome.tabs.create({ url: url_new_tab });
+        });
+        var removeIcon = link.querySelector(".fa-remove");
+        removeIcon.addEventListener("click", function (event) {
+          removeItemFromStoragebyIndex(index);
+        });
+        showLinkHere.appendChild(link);
+      });
+      // show to showNotAvailableLinkHere for index that is not on indextoshow
+      for (var i = 0; i < urlList.length; i++) {
+        if (!indexToShow.includes(i)) {
+          var link = document.createElement("div");
+          link.className = "list-group-item";
+          link.dataset.linkUrl = urlList[i].url;
+          link.dataset.linkTitle = urlList[i].title;
+          link.innerHTML = `<div class="d-flex justify-content-between align-items-center">
+          <div class="title-link">
+            ${urlList[i].title} ${i}
+          </div>
+          <div>
+            <i class="fa fa-remove fa-fw"></i>
+          </div>
+        </div>`;
+          var removeIcon = link.querySelector(".fa-remove");
+          removeIcon.addEventListener("click", (function (index) {
+            return function (event) {
+              console.log("event listener" + index);
+              removeItemFromStoragebyIndex(index);
+            };
+          })(i));
+          showNotAvailableLinkHere.appendChild(link);
+        }
+      }
+    }
+  });
+}
+
 
